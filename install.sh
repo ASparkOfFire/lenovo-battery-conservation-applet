e15320f54d11ce28d74cd4b19fbc8249913aa994
asparkoffire <kabir.47.chd@gmail.com>
2020-07-14 16:57:41 +0530

    initial commit
    Signed-off-by: asparkoffire <kabir.47.chd@gmail.com>

---
+++ install.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+REPO_URL="https://github.com/ASparkOfFire/lenovo-battery-conservation-applet.git"
+DEST_DIR="$PWD/lenovo-battery-conservation-applet"
+
+git clone $REPO_URL $DEST_DIR
+cd $DEST_DIR
+
+inst() {
+# copy to system
+cp -rf ./etc/xdg/autostart/lenovo-battery-conservation.desktop /etc/xdg/autostart/lenovo-battery-conservation.desktop;
+cp -rf ./usr/bin/conservation_mode /usr/bin/conservation_mode;
+cp -rf ./usr/lib/lenovo-battery-conservation/lenovo-battery-conservation /usr/lib/lenovo-battery-conservation/lenovo-battery-conservation;
+cp -rf ./usr/share/icons/hicolor/scalable/status/lenovo-logo.png /usr/share/icons/hicolor/scalable/status/lenovo-logo.png;
+cp -rf ./usr/share/icons/hicolor/scalable/status/conservation_off.svg /usr/share/icons/hicolor/scalable/status/conservation_off.svg;
+cp -rf ./usr/share/icons/hicolor/scalable/status/conservation_on.svg /usr/share/icons/hicolor/scalable/status/conservation_on.svg;
+
+# change ownership
+chown root:root /etc/xdg/autostart/lenovo-battery-conservation.desktop;
+chown root:root /usr/bin/conservation_mode;
+chown root:root /usr/lib/lenovo-battery-conservation/lenovo-battery-conservation;
+chown root:root /usr/lib/lenovo-battery-conservation;
+chown root:root /usr/share/icons/hicolor/scalable/status/lenovo-logo.png;
+chown root:root /usr/share/icons/hicolor/scalable/status/conservation_off.svg;
+chown root:root /usr/share/icons/hicolor/scalable/status/conservation_on.svg;
+
+# set permissions;
+
+chmod 0644 /etc/xdg/autostart/lenovo-battery-conservation.desktop;
+chmod 0755 /usr/bin/conservation_mode;
+chmod 0755 /usr/lib/lenovo-battery-conservation/lenovo-battery-conservation;
+chmod 0755 /usr/lib/lenovo-battery-conservation;
+chmod 0644 /usr/share/icons/hicolor/scalable/status/lenovo-logo.png;
+chmod 0644 /usr/share/icons/hicolor/scalable/status/conservation_off.svg;
+chmod 0644 /usr/share/icons/hicolor/scalable/status/conservation_on.svg; }
+
+sudo sh -c "$(declare -f inst); inst"
